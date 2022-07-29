ba1ee32412f41ae8c9ff0bf12d281b298f9d82d9
sunhak20 <sunhak20@naver.com>
2022-07-29 14:01:51 +0900

    BackJoon_1260

--- src/BackJoon_1260.java
+++ src/BackJoon_1260.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BackJon_1260 {
+public class BackJoon_1260 {
     public static boolean check[];
     public static int arr[][];
 
